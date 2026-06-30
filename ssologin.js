63c5a664f0e19d562fc4a358c2ba2606aa2ec956
Marti Alonso <marti.alonso.garcia@estudiantat.upc.edu>
2026-06-30 22:25:28 +0200

    Update to new login form

--- ssologin.js
+++ ssologin.js
@@ -1,24 +1,24 @@
 function login() {
-	if (document.getElementById('login_authn_error_msg') == null) {
+	if (document.getElementById('input-error') == null && document.getElementById('input-error-otp-code') == null) {
 		chrome.storage.local.get({active: false, username: '', password: '', secret2fa: ''}, function(items) {
 			if (items.active) {
 				var username = items.username;
 				var password = items.password;
 				var secret2fa = items.secret2fa;
 				
-				var code2fa = document.getElementById('input2factor');
+				var code2fa = document.getElementById('otp');
 				if (!code2fa) {
 					if (username != '') {
-						document.getElementById('edit-name').value = username;
-						document.getElementById('edit-pass').value = password;
-						document.getElementById('submit_ok').click();
+						document.getElementById('username').value = username;
+						document.getElementById('password').value = password;
+						document.getElementById('kc-login').click();
 					}
 				} else {
 					if (secret2fa != '') {
 						var totpObj = new TOTP();
 						var otp = totpObj.getOTP(secret2fa);
 						code2fa.value = otp;
-						document.getElementById('notification_2factor_button_ok').click();
+						document.getElementById('kc-login').click();
 					}
 				}
 			}
